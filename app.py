fc374c4f8bb0745e3845b489ea0afb801732bb35
Jongyoul Lee <jongyoul.lee@linecorp.com>
2023-09-21 19:44:56 +0900

    Add handler for `slack/events`

--- app.py
+++ app.py
@@ -11,5 +11,32 @@ def index():
     return {'message': 'Hello, folks!'}, 200
 
 
+@app.route('/slack/events', methods=['POST'])
+def slack_events():
+    data = request.json
+
+    event_type = data.get('type')
+
+    if event_type == 'url_verification':
+        challenge = data.get('challenge')
+        return jsonify({'challenge': challenge})
+
+    elif event_type == 'event_callback':
+        event = data.get('event')
+
+        if event['type'] == 'message':
+            response_text = "메시지 이벤트를 처리했습니다."
+        else:
+            response_text = "지원하지 않는 이벤트 타입입니다."
+
+        response = {
+            'text': response_text
+        }
+        return jsonify(response)
+
+    else:
+        return '', 200
+
+
 if __name__ == '__main__':
     app.run(debug=True)
